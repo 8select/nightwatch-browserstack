b586502c270564b082d184f6148e6cf835905205
Andreas Kleiber <andreas.kleiber@8select.de>
2017-09-26 11:03:23 +0200

    update browserstack session status

--- tests/single/single_test.js
+++ tests/single/single_test.js
@@ -1,4 +1,16 @@
 module.exports = {
+  beforeEach: function (browser, done) {
+    if (this.test_settings.selenium_host === 'hub.browserstack.com') {
+      return require('nightwatch-browserstack').storeSessionId(browser, done)
+    }
+    done()
+  },
+  afterEach: function (browser, done) {
+    if (this.test_settings.selenium_host === 'hub.browserstack.com') {
+      return require('nightwatch-browserstack').updateStatus(browser, done)
+    }
+    done()
+  },
   'Google\'s Search Functionality' : function (browser) {
     browser
       .url('https://www.google.com/ncr')
